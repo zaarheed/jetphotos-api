d52e1a960ec03dfc6506f3a5debe448b1fc157ec
Zahid Mahmood <zmahmood@global-aero.com>
2019-10-23 14:35:18 +0100

    clean up

--- config/env.js
+++ config/env.js
@@ -3,7 +3,5 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
 	cdnBaseUrl: process.env.CDN_BASE_URL,
-	environment: process.env.NODE_ENV,
-	lowDbBasePath: process.env.LOW_DB_BASE_PATH,
-	zabihahBaseUrl: process.env.ZABIHAH_BASE_URL
+	environment: process.env.NODE_ENV
 };
